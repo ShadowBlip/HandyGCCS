df5b53226b62383308722b00af1325346d4e1349
Derek J. Clark <derekjohn.clark@gmail.com>
2022-06-21 15:35:47 -0700

    Added installer.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+echo "Installing HandyGCCS..."
+sudo pacman -Sy --noconfirm - < pkg_depends.list
+< pip_depends.list xargs python3 -m pip install
+echo "Enabling controller functionality. NEXT users will need to configure the Home button in steam."
+cp -v handycon.py /usr/local/bin/
+cp -v handycon.service /etc/systemd/system/
+cp -v 60-handycon.rules /etc/udev/rules.d/
+systemctl enable handycon && systemctl start handycon
+echo "Installation complete. You should now have additional controller functionality."
+exit 0
---
+++ remove.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "Removing HandyGCCS..."
+systemctl stop handycon && systemctl disable handycon
+rm -v /usr/local/bin/handycon.py 
+rm -v /etc/systemd/system/handycon.service 
+rm -v /etc/udev/rules.d/60-handycon.rules 
+echo "Removal complete. Original configuration has been restored."
+exit 0
