d83c22f73349059b04e8a5396b7008bb4ecaff0d
Derek J. Clark <derekjohn.clark@gmail.com>
2022-07-18 19:59:42 -0700

    Fix install location

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo pacman -Sy --noconfirm - < pkg_depends.list
 echo "Enabling controller functionality. NEXT users will need to configure the Home button in steam."
 cp -v handycon.py /usr/local/bin/
 cp -v handycon.service /etc/systemd/system/
-cp -v handycon.conf /etc/modprobe.d/
+cp -v handycon.conf /etc/modules-load.d/
 cp -v 60-handycon.rules /etc/udev/rules.d/
 udevadm control -R
 systemctl enable handycon && systemctl start handycon
