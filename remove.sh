f3c5c895bd15895d81c994c7d081cf7b9f4d3c75
Derek J. Clark <derekjohn.clark@gmail.com>
2022-12-10 18:07:46 -0800

    Restructure to best unix practices. Uses /usr/share/handyggcs/scripts for executables. Uses /usr/lib for udev, modules-load, and systemd files.

--- remove.sh
+++ remove.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "Removing HandyGCCS..."
 systemctl stop handycon && systemctl disable handycon
-rm -v /usr/local/bin/constants.py
-rm -v /usr/local/bin/handycon.py 
-rm -v /etc/systemd/system/handycon.service 
-rm -v /etc/udev/rules.d/60-handycon.rules 
-rm -v /etc/modules-load.d/handycon.conf 
+rm -v /usr/share/handygccs/scripts/constants.py
+rm -v /usr/share/handygccs/scripts/handycon.py
+rm -v /usr/lib/systemd/system/handycon.service
+rm -v /usr/lib/udev/rules.d/60-handycon.rules
+rm -v /usr/lib/modules-load.d/handycon.conf
 udevadm control -R
 echo "Removal complete. Original configuration has been restored."
 exit 0
