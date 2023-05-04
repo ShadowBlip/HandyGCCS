5633c480264dd3fe4433981bea75a933e67db3cd
Derek J. Clark <derekjohn.clark@gmail.com>
2023-05-03 19:09:21 -0700

    fix namespace in gyro toggle

--- usr/share/handygccs/scripts/common.py
+++ usr/share/handygccs/scripts/common.py
@@ -673,7 +673,7 @@ async def emit_now(seed_event, event_list, value):
     await emit_events(events)
 
 async def toggle_gyro():
-    gyro_enabled = not com.gyro_enabled
+    gyro_enabled = not gyro_enabled
     if gyro_enabled:
         await do_rumble(0, 250, 1000, 0)
     else:
