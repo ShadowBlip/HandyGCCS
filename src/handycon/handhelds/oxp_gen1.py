372bf9d77a000632114add7c3412b9a56b8b4c24
Derek J. Clark <derekjohn.clark@gmail.com>
2023-06-11 16:35:26 -0700

    fix loop crash on oxp_gen1 with placeholder.

--- src/handycon/handhelds/oxp_gen1.py
+++ src/handycon/handhelds/oxp_gen1.py
@@ -37,7 +37,7 @@ async def process_event(seed_event, active_keys):
     button3 = handycon.button_map["button3"]  # Default ESC
     button4 = handycon.button_map["button4"]  # Default OSK
     button5 = handycon.button_map["button5"]  # Default MODE
-
+    button6 = ["Open Chimera"]
     ## Loop variables
     events = []
     this_button = None
