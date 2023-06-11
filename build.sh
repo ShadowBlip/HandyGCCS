247e91c2d7b1e7ec9a2efae64a7fe16a4b07f677
Derek J. Clark <derekjohn.clark@gmail.com>
2023-06-11 16:17:43 -0700

    Fix rebuild

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf build dist src/handycon.egg-info
-sudo rm -rf /usr/lib/python3.10/site-packages/handycon*
+sudo rm -rf /usr/lib/python3*/site-packages/handycon*
 sudo rm /usr/bin/handycon  
 python -m build --wheel --no-isolation
 sudo python -m installer dist/*.whl
