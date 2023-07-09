f27396d67d969c386e84bb1799348dfb8959f2f0
Derek J. Clark <derekjohn.clark@gmail.com>
2023-07-08 18:43:03 -0700

    Use event_queue for Ally

--- src/handycon/handhelds/ally_gen1.py
+++ src/handycon/handhelds/ally_gen1.py
@@ -48,108 +48,97 @@ async def process_event(seed_event, active_keys):
 
     ## Loop variables
     button_on = seed_event.value
+    this_button = None
 
     # BUTTON 1 (Default: Screenshot) Paddle + Y
     if active_keys == [184] and button_on == 1 and button1 not in handycon.event_queue:
         handycon.event_queue.append(button1)
-        await handycon.emit_now(seed_event, button1, 1)
     elif active_keys == [] and seed_event.code in [184] and button_on == 0 and button1 in handycon.event_queue:
-        handycon.event_queue.remove(button1)
-        await handycon.emit_now(seed_event, button1, 0)
+        this_button = button5
 
 
     # BUTTON 2 (Default: QAM) Armory Crate Button Short Press
     if active_keys == [148] and button_on == 1 and button2 not in handycon.event_queue:
         handycon.event_queue.append(button2)
-        await handycon.emit_now(seed_event, button2, 1)
     elif active_keys == [] and seed_event.code in [148] and button_on == 0 and button2 in handycon.event_queue:
-        handycon.event_queue.remove(button2)
-        await handycon.emit_now(seed_event, button2, 0)
+        this_button = button1
 
     # BUTTON 3 (Default: ESC) Paddle + B Temp disabled, goes nuts.
     # This event triggers from KEYBOARD_2.
     if active_keys == [49, 125] and button_on == 1 and button3 not in handycon.event_queue:
         handycon.event_queue.append(button3)
-        await handycon.emit_now(seed_event, button3, 1)
     elif active_keys == [] and seed_event.code in [49, 125] and button_on == 0 and button3 in handycon.event_queue:
-        handycon.event_queue.remove(button3)
-        await handycon.emit_now(seed_event, button3, 0)
+        this_button = button2
 
     # BUTTON 4 (Default: OSK) Paddle + D-Pad UP
     if active_keys == [88] and button_on == 1 and button4 not in handycon.event_queue:
         handycon.event_queue.append(button4)
-        await handycon.emit_now(seed_event, button4, 1)
     elif active_keys == [] and seed_event.code in [88] and button_on == 0 and button4 in handycon.event_queue:
-        handycon.event_queue.remove(button4)
-        await handycon.emit_now(seed_event, button4, 0)
+        this_button = button3
 
     # BUTTON 5 (Default: GUIDE) Ally Home Short Press.
     if active_keys == [186] and button_on == 1 and button5 not in handycon.event_queue:
         handycon.event_queue.append(button5)
-        await handycon.emit_now(seed_event, button5, 1)
     elif active_keys == [] and seed_event.code in [186] and button_on == 0 and button5 in handycon.event_queue:
-        handycon.event_queue.remove(button5)
-        await handycon.emit_now(seed_event, button5, 0)
+        this_button = button4
 
     # BUTTON 6 (Default: Launch Chimera. Paddle + A
     if active_keys == [68] and button_on == 1 and button6 not in handycon.event_queue:
         handycon.event_queue.append(button6)
-        await handycon.emit_now(seed_event, button6, 1)
     elif active_keys == [] and seed_event.code in [68] and button_on == 0 and button6 in handycon.event_queue:
-        handycon.event_queue.remove(button6)
-        await handycon.emit_now(seed_event, button6, 0)
+        this_button = button6
 
     # BUTTON 7 (Default: Toggle Performance) Armory Crate Button Long Press
     # This button triggers immediate down/up after holding for ~1s an F17 and then
     # released another down/up for F18 on release. We use the F18 "KEY_UP" for release.
     if active_keys == [187] and button_on == 1 and button7 not in handycon.event_queue:
         handycon.event_queue.append(button7)
-        await handycon.emit_now(seed_event, button7, 1)
     elif active_keys == [] and seed_event.code in [188] and button_on == 0 and button7 in handycon.event_queue:
-        handycon.event_queue.remove(button7)
-        await handycon.emit_now(seed_event, button7, 0)
+        this_button = button7
 
     # BUTTON 8 (Default: GUIDE) Ally Home Long Press.
     # This event triggers from KEYBOARD_2.
     if active_keys == [29, 56, 111] and button_on == 1 and button8 not in handycon.event_queue:
         handycon.event_queue.append(button8)
-        await handycon.emit_now(seed_event, button8, 1)
     elif active_keys == [] and seed_event.code in [29, 56, 111] and button_on == 0 and button8 in handycon.event_queue:
-        handycon.event_queue.remove(button8)
-        await handycon.emit_now(seed_event, button8, 0)
+        this_button = button8
 
     # BUTTON 9 (Default:) Paddle + D-Pad DOWN
     # This event triggers from KEYBOARD_2.
     if active_keys == [1, 29, 42] and button_on == 1 and button9 not in handycon.event_queue:
         handycon.event_queue.append(button9)
-        await handycon.emit_now(seed_event, button9, 1)
     elif active_keys == [] and seed_event.code in [1, 29, 42] and button_on == 0 and button9 in handycon.event_queue:
-        handycon.event_queue.remove(button9)
-        await handycon.emit_now(seed_event, button9, 0)
+        this_button = button9
 
     # BUTTON 10 (Default:) Paddle + D-Pad LEFT
     # This event triggers from KEYBOARD_2.
     if active_keys == [32, 125] and button_on == 1 and button10 not in handycon.event_queue:
         handycon.event_queue.append(button10)
-        await handycon.emit_now(seed_event, button10, 1)
     elif active_keys == [] and seed_event.code in [32, 125] and button_on == 0 and button10 in handycon.event_queue:
-        handycon.event_queue.remove(button10)
-        await handycon.emit_now(seed_event, button10, 0)
+        this_button = button10
 
     # BUTTON 11 (Default:) Paddle + D-Pad RIGHT
     # This event triggers from KEYBOARD_2.
     if active_keys == [15, 125] and button_on == 1 and button11 not in handycon.event_queue:
         handycon.event_queue.append(button11)
-        await handycon.emit_now(seed_event, button11, 1)
     elif active_keys == [] and seed_event.code in [15, 125] and button_on == 0 and button11 in handycon.event_queue:
-        handycon.event_queue.remove(button11)
-        await handycon.emit_now(seed_event, button11, 0)
+        this_button = button11
 
     # BUTTON 12 (Default:) Paddle + X
     # This event triggers from KEYBOARD_2.
     if active_keys == [25, 125] and button_on == 1 and button12 not in handycon.event_queue:
         handycon.event_queue.append(button12)
-        await handycon.emit_now(seed_event, button12, 1)
     elif active_keys == [] and seed_event.code in [25, 125] and button_on == 0 and button12 in handycon.event_queue:
-        handycon.event_queue.remove(button12)
-        await handycon.emit_now(seed_event, button12, 0)
+        this_button = button12
+
+    # Create list of events to fire.
+    # Handle new button presses.
+    if this_button and not handycon.last_button:
+        handycon.event_queue.remove(this_button)
+        handycon.last_button = this_button
+        await handycon.emit_now(seed_event, this_button, 1)
+
+    # Clean up old button presses.
+    elif handycon.last_button and not this_button:
+        await handycon.emit_now(seed_event, handycon.last_button, 0)
+        handycon.last_button = None
