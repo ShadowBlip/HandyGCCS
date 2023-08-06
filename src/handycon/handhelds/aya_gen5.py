a1d7b37757df2d86314889375581475c5011b50e
Derek J. Clark <derekjohn.clark@gmail.com>
2023-08-05 21:26:54 -0700

    Cant use imported function, handycon doesn;t get set.

--- src/handycon/handhelds/aya_gen5.py
+++ src/handycon/handhelds/aya_gen5.py
@@ -7,7 +7,6 @@ from asyncio import sleep
 from evdev import InputDevice, InputEvent, UInput, ecodes as e, list_devices, ff
 
 from .. import constants as cons
-from .aya_gen4 import process_event
 handycon = None
 
 def init_handheld(handheld_controller):
@@ -21,3 +20,53 @@ def init_handheld(handheld_controller):
     handycon.GAMEPAD_NAME = 'Microsoft X-Box 360 pad'
     handycon.KEYBOARD_ADDRESS = 'isa0060/serio0/input0'
     handycon.KEYBOARD_NAME = 'AT Translated Set 2 keyboard'
+
+
+# Captures keyboard events and translates them to virtual device events.
+async def process_event(seed_event, active_keys):
+    global handycon
+
+    # Button map shortcuts for easy reference.
+    button1 = handycon.button_map["button1"]  # Default Screenshot
+    button2 = handycon.button_map["button2"]  # Default QAM
+    button3 = handycon.button_map["button3"]  # Default ESC
+    button4 = handycon.button_map["button4"]  # Default OSK
+    button5 = handycon.button_map["button5"]  # Default MODE
+
+    ## Loop variables
+    button_on = seed_event.value
+
+    # Automatically pass default keycodes we dont intend to replace.
+    if seed_event.code in [e.KEY_VOLUMEDOWN, e.KEY_VOLUMEUP]:
+        await handycon.emit_events([seed_event])
+
+    # BUTTON 1 (Default: Screenshot/Launch Chiumera) LC Button
+    if active_keys == [29, 125, 185] and button_on == 1 and button1 not in handycon.event_queue:
+        await handycon.handle_key_down(seed_event, button1)
+    elif active_keys == [] and seed_event.code in [29, 125, 185] and button_on == 0 and button1 in handycon.event_queue:
+        await handycon.handle_key_up(seed_event, button1)
+
+    # BUTTON 2 (Default: QAM) Small Button
+    if active_keys == [32, 125] and button_on == 1 and button2 not in handycon.event_queue:
+        await handycon.handle_key_down(seed_event, button2)
+    elif active_keys == [] and seed_event.code in [32, 125] and button_on == 0 and button2 in handycon.event_queue:
+        await handycon.handle_key_up(seed_event, button2)
+
+    # BUTTON 4 (Default: OSK) RC Button
+    if active_keys == [29, 125, 186] and button_on == 1 and button4 not in handycon.event_queue:
+        await handycon.handle_key_down(seed_event, button4)
+    elif active_keys == [] and seed_event.code in [29, 125, 186] and button_on == 0 and button4 in handycon.event_queue:
+        await handycon.handle_key_up(seed_event, button4)
+
+    # BUTTON 5 (Default: MODE) Big button
+    if active_keys == [29, 125, 187] and button_on == 1 and button5 not in handycon.event_queue:
+        await handycon.handle_key_down(seed_event, button5)
+    elif active_keys == [] and seed_event.code in [29, 125, 187] and button_on == 0 and button5 in handycon.event_queue:
+        await handycon.handle_key_up(seed_event, button5)
+
+    # Handle L_META from power button
+    elif active_keys == [] and seed_event.code == 125 and button_on == 0 and handycon.event_queue == [] and handycon.shutdown == True:
+        handycon.shutdown = False
+
+    if handycon.last_button:
+        await handycon.handle_key_up(seed_event, handycon.last_button)
