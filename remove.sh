6e8254f123a277c6e727f7d090d9a1e585203979
Derek J. Clark <derekjohn.clark@gmail.com>
2023-10-25 19:08:49 -0700

    remove delete of handygccs config during reinstall.

--- remove.sh
+++ remove.sh
@@ -3,7 +3,6 @@ rm -rf build dist src/handycon.egg-info
 sudo rm -rf /usr/lib/python3*/site-packages/handycon*
 sudo rm /usr/bin/handycon
 sudo rm -rf /usr/share/handygccs
-sudo rm -rf /etc/handygccs
 sudo rm /usr/lib/systemd/system/handycon.service
 sudo rm /usr/lib/udev/hwdb.d/59-handygccs-ayaneo.hwdb
 sudo rm /usr/lib/udev/rules.d/60-handycon.rules
