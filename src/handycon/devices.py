07521b8598b52ce99660f0c6110676beb7a3e9e5
Derek J. Clark <derekjohn.clark@gmail.com>
2024-01-09 19:07:17 -0500

    Add GPD WIN Mini.

--- src/handycon/devices.py
+++ src/handycon/devices.py
@@ -27,6 +27,7 @@ import handycon.handhelds.go_gen1 as go_gen1
 import handycon.handhelds.gpd_gen1 as gpd_gen1
 import handycon.handhelds.gpd_gen2 as gpd_gen2
 import handycon.handhelds.gpd_gen3 as gpd_gen3
+import handycon.handhelds.gpd_gen4 as gpd_gen4
 import handycon.handhelds.oxp_gen1 as oxp_gen1
 import handycon.handhelds.oxp_gen2 as oxp_gen2
 import handycon.handhelds.oxp_gen3 as oxp_gen3
@@ -284,6 +285,8 @@ async def capture_keyboard_events():
                             await gpd_gen2.process_event(seed_event, active_keys)
                         case "GPD_GEN3":
                             await gpd_gen3.process_event(seed_event, active_keys)
+                        case "GPD_GEN4":
+                            await gpd_gen4.process_event(seed_event, active_keys)
                         case "OXP_GEN1":
                             await oxp_gen1.process_event(seed_event, active_keys)
                         case "OXP_GEN2":
---
+++ src/handycon/handhelds/gpd_gen4.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# This file is part of Handheld Game Console Controller System (HandyGCCS)
+# Copyright 2022-2023 Derek J. Clark <derekjohn.clark@gmail.com>
+
+from evdev import InputDevice, InputEvent, UInput, ecodes as e, list_devices, ff
+
+handycon = None
+
+def init_handheld(handheld_controller):
+    global handycon
+    handycon = handheld_controller
+    handycon.BUTTON_DELAY = 0.11
+    handycon.CAPTURE_CONTROLLER = True
+    handycon.CAPTURE_KEYBOARD = True
+    handycon.CAPTURE_POWER = True
+    handycon.GAMEPAD_ADDRESS = 'usb-0000:63:00.3-5/input0'
+    handycon.GAMEPAD_NAME = 'Microsoft X-Box 360 pad'
+    handycon.KEYBOARD_ADDRESS = 'usb-0000:63:00.3-3/input1'
+    handycon.KEYBOARD_NAME = '  Mouse for Windows'
+
+async def process_event(seed_event, active_keys):
+    global handycon
+
+    # Button map shortcuts for easy reference.
+    button1 = handycon.button_map["button1"]  # Default Screenshot
+    button2 = handycon.button_map["button2"]  # Default QAM
+
+    ## Loop variables
+    button_on = seed_event.value
+
+    # Automatically pass default keycodes we dont intend to replace.
+    if seed_event.code in [e.KEY_VOLUMEDOWN, e.KEY_VOLUMEUP]:
+        handycon.emit_event(seed_event)
+
+    # BUTTON 1 (Default: Screenshot)
+    if active_keys == [119] and button_on == 1 and button1 not in handycon.event_queue:
+        await handycon.handle_key_down(seed_event, button1)
+    elif active_keys == [] and seed_event.code in [119] and button_on == 0 and button1 in handycon.event_queue:
+        await handycon.handle_key_up(seed_event, button1)
+
+    # BUTTON 2 (Default: QAM)
+    if active_keys == [99] and button_on == 1 and button2 not in handycon.event_queue:
+        await handycon.handle_key_down(seed_event, button2)
+    elif active_keys == [] and seed_event.code in [99] and button_on == 0 and button2 in handycon.event_queue:
+        await handycon.handle_key_up(seed_event, button2)
+
+    # Handle L_META from power button
+    elif active_keys == [] and seed_event.code == 125 and button_on == 0 and  handycon.event_queue == [] and handycon.shutdown == True:
+        handycon.shutdown = False
+
+    if handycon.last_button:
+        await handycon.handle_key_up(seed_event, handycon.last_button)
