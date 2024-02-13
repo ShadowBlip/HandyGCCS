6d02295d98d916315efa09d7f7d3adedc6ea7ec9
Derek J. Clark <derekjohn.clark@gmail.com>
2024-02-12 20:54:36 -0800

    Remvoe extra key.

--- src/handycon/handhelds/aya_gen2.py
+++ src/handycon/handhelds/aya_gen2.py
@@ -26,11 +26,9 @@ async def process_event(seed_event, active_keys):
 
     # Button map shortcuts for easy reference.
     button2 = handycon.button_map["button2"]  # Default QAM
-    button4 = handycon.button_map["button4"]  # Default OSK
     button5 = handycon.button_map["button5"]  # Default MODE
 
     # Loop variables
-    this_button = None
     button_on = seed_event.value
 
     # Automatically pass default keycodes we dont intend to replace.
@@ -39,36 +37,24 @@ async def process_event(seed_event, active_keys):
 
     # BUTTON 2 (Default: QAM) Small Button Short Press
     if active_keys in [[40, 133], [32, 125]] and button_on == 1 and button2 not in handycon.event_queue:
-        handycon.event_queue.append(button2)
-    elif active_keys == [] and seed_event.code in [32, 40] and button_on == 0 and button2 in handycon.event_queue:
-        this_button = button2
+        await handycon.handle_key_down(seed_event, button2)
+    elif active_keys == [] and seed_event.code in [32, 40, 125, 133] and button_on == 0 and button2 in handycon.event_queue:
+        await handycon.handle_key_up(seed_event, button2)
 
-    # BUTTON 4 (Default: OSK) Short press KB
-    if active_keys == [97, 100, 111] and button_on == 1 and button4 not in handycon.event_queue:
-        if button2 in handycon.event_queue:
-            handycon.event_queue.remove(button2)
-        handycon.event_queue.append(button4)
-    elif active_keys == [] and seed_event.code in [100, 111] and button_on == 0 and button4 in handycon.event_queue:
-        this_button = button4
+    # BUTTON 4 (Default: OSK) Small button Long Press
+    # This button spams up/down events, useless for now.
+    # active_keys == [97, 100, 111]
 
     # BUTTON 5 (Default: MODE) Big button
     if active_keys in [[96, 105, 133], [88, 97, 125]] and button_on == 1 and button5 not in handycon.event_queue:
         await handycon.handle_key_down(seed_event, button5)
-    elif active_keys == [] and seed_event.code in [88, 96, 105] and button_on == 0 and button5 in handycon.event_queue:
+    elif active_keys == [] and seed_event.code in [88, 96, 97, 105, 125, 133] and button_on == 0 and button5 in handycon.event_queue:
         await handycon.handle_key_up(seed_event, button5)
 
     # Handle L_META from power button
-    elif active_keys == [] and seed_event.code == 125 and button_on == 0 and handycon.event_queue == [] and handycon.shutdown == True:
+    if active_keys == [] and seed_event.code == 125 and button_on == 0 and handycon.event_queue == [] and handycon.shutdown == True:
         handycon.shutdown = False
 
-    # Create list of events to fire.
-    # Handle new button presses.
-    if this_button and not handycon.last_button:
-        handycon.event_queue.remove(this_button)
-        handycon.last_button = this_button
-        await handycon.emit_now(seed_event, this_button, 1)
-
     # Clean up old button presses.
-    elif handycon.last_button and not this_button:
-        await handycon.emit_now(seed_event, handycon.last_button, 0)
-        handycon.last_button = None
+    if handycon.last_button:
+        await handycon.handle_key_up(seed_event, handycon.last_button)
